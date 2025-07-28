65cb43fef62208191368f5b2af8e9dd55847ef32
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-28 23:00:35 +0200

    Adding useState,creating input and button, and simple loading logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
 import './index.css'
 
 function App() {
+  const [text,setText]=useState<string>('')
+  const [textTypeWrite,setTextTypeWrite]=useState<string[]>()
+  const handleClick=()=>{
+    setTextTypeWrite(text.split(''))
+    setText('')
+  }
 
   return (
-    <div className='bg-red-500'>hello</div>
+    <div className=' p-5'>
+      <input className='w-2/7 border rounded-sm p-1' value={text} onChange={e=>setText(e.target.value) }/>
+      <button className='ml-5 border p-1 rounded-xl bg-gray-300 cursor-pointer' onClick={handleClick}>Display with typewriter effect</button>
+      {textTypeWrite ? <div className='flex flex-row '>
+        
+        {textTypeWrite.map((t)=>{
+          return <span>{t}</span>
+        })}
+      </div>:null}
+    </div>
   )
 }
 
