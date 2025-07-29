fa20646414b18eead271627c3bb82428b70d01c3
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-29 20:23:37 +0200

    [final Commit] Disabling button to prevent couple timeouts running simultaneously,

--- src/TypeWriter.tsx
+++ src/TypeWriter.tsx
@@ -4,8 +4,11 @@ import './index.css'
 export default function Typewriter() {
    let i=0;
   const [text,setText]=useState<string>('')
+  const [disabled,setDisabled]=useState(false)
   const [textTypeWrite,setTextTypeWrite]=useState<string[]>([])
   const handleClick=()=>{
+    if(!text|| disabled) return;
+    setDisabled(true)
     setText('')
     setTextTypeWrite([])
     repeat()
@@ -15,13 +18,15 @@ function repeat() {
   if(text.length>=i){
       setTextTypeWrite((t)=>[...t,text[i-1]])
       setTimeout(repeat,500);
+    }else{
+      setDisabled(false)
     }
   i++;
 }
   return (
     <div className=' p-5'>
       <input className='w-2/7 border rounded-sm p-1' value={text} onChange={e=>setText(e.target.value) }/>
-      <button className='ml-5 border p-1 rounded-xl bg-gray-300 cursor-pointer' onClick={handleClick}>Display with typewriter effect</button>
+      <button className={`ml-5 border p-1 rounded-xl bg-gray-300 ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`} onClick={handleClick}>Display with typewriter effect</button>
       {textTypeWrite ? <div className='flex flex-row '>
         
         {textTypeWrite}
