f9e9c41a543655c4c8431b2a985757a84918d750
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-29 20:03:19 +0200

    Setting up repeat function, restructuring code, using setTimeout instead of setInterval(), and moving all logic to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useState } from 'react'
+import Typewriter from './TypeWriter'
 import './index.css'
 
 function App() {
-  const [text,setText]=useState<string>('')
-  const [textTypeWrite,setTextTypeWrite]=useState<string[]>()
-  const handleClick=()=>{
-    setTextTypeWrite(text.split(''))
-    setText('')
-  }
-
+  
   return (
-    <div className=' p-5'>
-      <input className='w-2/7 border rounded-sm p-1' value={text} onChange={e=>setText(e.target.value) }/>
-      <button className='ml-5 border p-1 rounded-xl bg-gray-300 cursor-pointer' onClick={handleClick}>Display with typewriter effect</button>
-      {textTypeWrite ? <div className='flex flex-row '>
-        
-        {textTypeWrite.map((t)=>{
-          return <span>{t}</span>
-        })}
-      </div>:null}
+    <div>
+      <Typewriter/>
     </div>
   )
 }
---
+++ src/TypeWriter.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './index.css'
+
+export default function Typewriter() {
+   let i=0;
+  const [text,setText]=useState<string>('')
+  const [textTypeWrite,setTextTypeWrite]=useState<string[]>([])
+  const handleClick=()=>{
+    setText('')
+    setTextTypeWrite([])
+    repeat()
+  }
+ 
+function repeat() {
+  if(text.length>=i){
+      setTextTypeWrite((t)=>[...t,text[i-1]])
+      setTimeout(repeat,500);
+    }
+  i++;
+}
+  return (
+    <div className=' p-5'>
+      <input className='w-2/7 border rounded-sm p-1' value={text} onChange={e=>setText(e.target.value) }/>
+      <button className='ml-5 border p-1 rounded-xl bg-gray-300 cursor-pointer' onClick={handleClick}>Display with typewriter effect</button>
+      {textTypeWrite ? <div className='flex flex-row '>
+        
+        {textTypeWrite}
+      </div>:null}
+    </div>
+  )
+}
+
+
